645ab0ca1a1a9185471f09b98f94d6160196e5c1
GhostNear <ghostnear1841@gmail.com>
2024-04-15 13:59:39 +0300

    Labels for graph

--- Business Ads/Frontend/MainWindow.xaml.cs
+++ Business Ads/Frontend/MainWindow.xaml.cs
@@ -1,7 +1,4 @@
-﻿using ScottPlot;
-using System.Collections;
-using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 
 namespace Frontend
 {
@@ -20,17 +17,17 @@ namespace Frontend
 
                 double[] dataX = new double[24];
                 for(int i = 1; i < 24; i++)
-                {
                     dataX[i] = i;
-                }
 
                 double[] dataY = new double[24];
-                for(int i = 1; i < 24; i++)
-                {
+                for(int i = 0; i < 24; i++)
                     dataY[i] =  r.Next(10);
-                }
 
-                EngagementPlot.Plot.Axes.SetLimitsX(left: 0, right: 24);
+                EngagementPlot.Plot.Title("Ad Engagement throughout the day");
+                EngagementPlot.Plot.XLabel("Hour of the day");
+                EngagementPlot.Plot.YLabel("Engagement on a scale from 1 to 10");
+
+                EngagementPlot.Plot.Axes.SetLimitsX(left: -0.5, right: 23.5);
                 EngagementPlot.Plot.Axes.SetLimitsY(bottom: 0, top: 10);
                 EngagementPlot.Plot.Add.Bars(dataX, dataY);
                 EngagementPlot.Refresh();
