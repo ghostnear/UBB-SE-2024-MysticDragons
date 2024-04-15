42b98009f271468ad742868c081e5c9e35e0dc1f
GhostNear <ghostnear1841@gmail.com>
2024-04-15 13:17:28 +0300

    Scotting plotting

--- Business Ads/Frontend/MainWindow.xaml.cs
+++ Business Ads/Frontend/MainWindow.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Frontend
 {
@@ -19,6 +10,14 @@ namespace Frontend
         public MainWindow()
         {
             InitializeComponent();
+
+            Loaded += (s, e) =>
+            {
+                double[] dataX = { 1, 2, 3, 4, 5 };
+                double[] dataY = { 1, 4, 9, 16, 25 };
+                WpfPlot1.Plot.Add.Scatter(dataX, dataY);
+                WpfPlot1.Refresh();
+            };
         }
     }
 }
