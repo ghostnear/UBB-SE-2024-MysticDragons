6490b3cee038458ed49d921c7dd1dd4b56826716
Ostapciuc Diana Miruna <diana.ostapciuc@gmail.com>
2024-04-16 10:30:58 +0300

    beginning of faq - not completely done

---
+++ Business Ads/Frontend/FAQ/ReviewClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frontend.FAQ
+{
+    internal class ReviewClass
+    {
+        private string _user;
+        private string _review;
+
+        public ReviewClass(string user, string review)
+        {
+            this._user = user;
+            this._review = review;
+        }
+
+        public string User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+        public string Review
+        {
+            get { return _review; }
+            set { _review = value; }
+        }
+
+        public override string ToString()
+        {
+            return $"{this._review}";
+        }
+    }
+}
