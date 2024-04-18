b05a043d1b52140c675de0ff99650e38871db0c4
Vlad <vplavosin@gmail.com>
2024-04-18 15:04:55 +0300

    more export functionality

--- Business Ads/Frontend/Export/ExportWindow.xaml.cs
+++ Business Ads/Frontend/Export/ExportWindow.xaml.cs
@@ -22,6 +22,9 @@ using System.Globalization;
 using System.IO;
 using CsvHelper;
 using Syncfusion.Pdf.Parsing;
+using System.Net.Mail;
+using System.Net;
+using System.Windows.Media.Media3D;
 
 namespace Frontend
 {
@@ -33,17 +36,31 @@ namespace Frontend
         public Window mainWindow;
         public class Stats
         {
+            public Stats(int views,int clicks,int buys,int time)
+            {
+                this.views = views;
+                this.time = time;
+                this.clicks = clicks;
+                this.buys = buys;
+            }
             public int views { get; set; }
             public int clicks { get; set; }
             public int buys { get; set; }
             public int time { get; set; }
         }
+        Stats stats = new Stats(1000,100,5,30);
+        
+
         public class User
         {
+            public User(String name)
+            {
+                this.Name = name;
+            }
             public String Name { get; set; }
 
         }
-        
+        User user = new User("Andrew Stone");
 
         public ExportWindow()
         {
@@ -66,7 +83,6 @@ namespace Frontend
             try
             {
                 float fontSize = float.Parse(SizeInput.Text);
-                //PdfFontFamily fontFamily = GetFontFamilyFromTextBox(FontInput.Text);
             }
             catch
             {
@@ -74,58 +90,99 @@ namespace Frontend
             }
             if (Radio7.IsChecked == true)
             {
+                PdfDocument document = new PdfDocument();
+                PdfPage page = document.Pages.Add();
+                PdfGraphics graphics = page.Graphics;
+                PdfBrush color = new PdfSolidBrush(System.Drawing.Color.Black);
+                int height = 20;
 
-                //Save the document
+                if (ColorBox.SelectedIndex == 1)
+                    color = new PdfSolidBrush(System.Drawing.Color.Gray);
+                if (ColorBox.SelectedIndex == 2)
+                    color = new PdfSolidBrush(System.Drawing.Color.Red);
+                graphics.DrawString("Advertisement Statistics Report", font, color, new PointF((page.Size.Width/2f) - 100, 10));
+
+                if (ImpressionsCheck.IsChecked == true)
+                {
+                    graphics.DrawString("\nAmount of Impressions: " + stats.views.ToString(), font, color, new PointF(10, height));
+                    height += 20;
+                }
+                if (ClicksCheck.IsChecked == true) { 
+                    graphics.DrawString("\nAmount of Clicks: " + stats.clicks.ToString(), font, color, new PointF(10, height));
+                height += 20;
+            }
+            if (BuysCheck.IsChecked == true) { 
+                    graphics.DrawString("\nAmount of Purchases: " + stats.buys.ToString(), font, color, new PointF(10, height));
+            height += 20;
+        }
+                if (TimeCheck.IsChecked == true) { 
+                    graphics.DrawString("\nTotal Time Viewed: " + stats.time.ToString(), font, color, new PointF(10, height));
+        height += 20;
+                }
+                if (CTRCheck.IsChecked == true) { 
+                    graphics.DrawString("\nClick Through Ratio: " + ((float)(stats.clicks/stats.views)).ToString(), font, color, new PointF(10, height));
+    height += 20;
+                }
+                if (DateCheck.IsChecked == true)
+                { 
+                    graphics.DrawString("Created on:", font, color, new PointF(20, page.Size.Height-120));
+                    graphics.DrawString(DateTime.Now.ToString(), font, color, new PointF(20, page.Size.Height - 100));
+                }
+                if (SignatureCheck.IsChecked == true)
+                {
+                    graphics.DrawString("Signature:", font, color, new PointF(page.Size.Width-200, page.Size.Height - 120));
+                    graphics.DrawString(user.Name, font, color, new PointF(page.Size.Width - 200, page.Size.Height - 100));
+                }
+                if (RecipientCheck.IsChecked == true)
+                {
+                    graphics.DrawString("Intended Recipient:", font, color, new PointF((page.Size.Width/2f)-50, page.Size.Height - 120));
+                    graphics.DrawString(RecipientInput.Text, font, color, new PointF((page.Size.Width / 2f)-50, page.Size.Height - 100));
+                }
+
                 if (DownloadButton1.IsChecked == true)
                 {
-                    PdfDocument document = new PdfDocument();
-
-                    // Add a page to the document
-                    PdfPage page = document.Pages.Add();
-
-                    // Create a PdfGraphics object for drawing
-                    PdfGraphics graphics = page.Graphics;
-
-                    if(ColorBox.SelectedIndex == 0)
-                    graphics.DrawString("Hello, Syncfusion PDF!", font, PdfBrushes.Black, new PointF(10, 10));
-                    if (ColorBox.SelectedIndex == 1)
-                        graphics.DrawString("Hello, Syncfusion PDF!", font, PdfBrushes.Gray, new PointF(10, 10));
-                    if (ColorBox.SelectedIndex == 2)
-                        graphics.DrawString("Hello, Syncfusion PDF!", font, PdfBrushes.Red, new PointF(10, 10));
-
-                    // Save the PDF document to a file
                     string outputPath = "C:\\Users\\User\\Downloads\\output.pdf";
                     document.Save(outputPath);
-
-                    // Close the document
-                    document.Close(true);
+                    
 
                 }
-                    if (EmailButton.IsChecked == true)
-                    {
-
-                    }
-                
-                
-                }
-            if (Radio6.IsChecked == true)
-            {
-                if (DownloadButton1.IsChecked == true)
+                if (EmailButton1.IsChecked == true)
                 {
-                    string someText = "test";
-                    File.WriteAllText(@".\csc.txt", someText);
+                    var mail = "mysticdragonsubb@gmail.com";
+                    var pass = "Password!123";
+
+                    var client = new SmtpClient("smtp.gmail.com", 587);
+                    {
+                        client.EnableSsl = true;
+                        client.Credentials = new NetworkCredential(mail, pass);
+                    }
+                    client.SendMailAsync(new MailMessage(from:mail,to:EmailInput1.Text,"Statistics Report","Attached you will find your report"));
                 }
+                document.Close(true);
             }
                     if (Radio5.IsChecked == true)
             {
                 if (DownloadButton1.IsChecked == true)
                 {
                     using (var writer = new StreamWriter("C:\\Users\\User\\Downloads\\output.csv"))
-
                     using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
                     {
-                        List<Stats> records = [new Stats { views = 1, clicks = 3, buys = 2, time = 1 }];
-                        csv.WriteRecords(records);
+                        List<Stats> records = new List<Stats> { stats };
+
+                        foreach (var record in records)
+                        {
+                            if(ImpressionsCheck.IsChecked == true)
+                            csv.WriteField(record.views);
+                            if (ClicksCheck.IsChecked == true)
+                                csv.WriteField(record.clicks);
+                            if (BuysCheck.IsChecked == true)
+                                csv.WriteField(record.buys);
+                            if (CTRCheck.IsChecked == true)
+                                csv.WriteField(((float)record.views/record.clicks));
+                            if (TimeCheck.IsChecked == true)
+                                csv.WriteField(record.time);
+                            csv.NextRecord();
+                        }
                     }
                 }
             }
